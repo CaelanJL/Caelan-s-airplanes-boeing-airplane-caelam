cc13141da9e1d4561136e13d975d8963a1905062
VegasAE <S3947138@student.rmit.edu.au>
2022-11-03 10:57:56 +1100

    search form

--- boing/src/pages/Search.jsx
+++ boing/src/pages/Search.jsx
@@ -2,7 +2,36 @@ import React from 'react'
 
 function SearchForm(){
     return (
-        <div></div>
+        <form>
+            <div class = 'row'>
+                <div class = 'form-group col-md-6'>
+                    <label for = 'operatorName'>Operator</label>
+                    <input type = 'text' class = 'form-control' id = 'operatorName' placeholder = 'e.g. George'></input>
+                </div>
+                <div class = 'form-group col-md-6'>
+                    <label for = 'acNUM'>Autoclave Number</label>
+                    <select id = 'acNum'>
+                        <option selected>Choose Autoclave</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                    </select>
+                </div>
+            </div>
+            <div class = 'row'>
+                <div class = 'form-group col-md-6'>
+                    <label for = 'loadNUM'>Load Number</label>
+                    <input type = 'number' id = 'loadNUM' class = 'form-control' pattern = '\b\d{5}\b' placeholder='e.g. 98543'></input>
+                </div>
+                <div class = 'form-group col-md-6'>
+                    <label for = 'partNUM'>Part Number</label>
+                    <input type = 'text' id = 'loadNUM' class = 'form-control' placeholder='e.g. ACME-111'></input>
+                </div>
+            </div>
+            <div class="d-grid gap-2">
+                <button type = 'button' class = 'btn btn-primary'>Filter</button>
+            </div>
+        </form>
     )
 }
 
