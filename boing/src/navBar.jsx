bd012b994c8a30e7d9389d844795ad51cf48d5cd
VegasAE <S3947138@student.rmit.edu.au>
2022-11-02 15:25:50 +1100

    got rid of useless imports

--- boing/src/navBar.jsx
+++ boing/src/navBar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 
 function Navbar() {
