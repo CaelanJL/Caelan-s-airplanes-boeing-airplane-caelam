10a0da3f0a40d88ed50c093f235ea96be3894726
CaelanJL <S3946941@student.rmit.edu.au>
2022-11-02 15:24:19 +1100

    he means nothing

--- boing/src/App.js
+++ boing/src/App.js
@@ -11,7 +11,137 @@ function App() {
   }
 
   function inputForm() {
+    return(
+    // <Form>
+    //   <Form.Group className="mb-3" controlId="formBasicEmail">
+    //     <Form.Label>Email address</Form.Label>
+    //     <Form.Control type="email" placeholder="Enter email" />
+    //     <Form.Text className="text-muted">
+    //       We'll never share your email with anyone else.
+    //     </Form.Text>
+    //   </Form.Group>
+
+    //   <Form.Group className="mb-3" controlId="formBasicPassword">
+    //     <Form.Label>Password</Form.Label>
+    //     <Form.Control type="password" placeholder="Password" />
+    //   </Form.Group>
+    //   <Form.Group className="mb-3" controlId="formBasicCheckbox">
+    //     <Form.Check type="checkbox" label="Check me out" />
+    //   </Form.Group>
+    //   <Button variant="primary" type="submit">
+    //     Submit
+    //   </Button>
+    // </Form>
+    <div>
+      <label class="form-label">Input JSON file</label>
+      <form class="mb-3 input-group">      
+        <input type="file" class="form-control" id="formFile" accept=".json"></input>
+        <button class="btn btn-outline-secondary" type="button" id="button-addon2">Submit</button>
+      </form>
+
+      <h3>OR</h3>
+
+      
+      
+
+      <form class="mb-3 container"> 
+      <label class="form-label">Input manually</label>
+
+        <div className="row">
+
+          <div class="input-group col">
+            <div class="input-group-prepend">
+              <span class="input-group-text" >nombar</span>
+            </div>
+            <input class="" type="text" placeholder="AC2-07337-anon"></input>
+          </div>
+
+          <div class="input-group col">
+            <div class="input-group-prepend">
+              <span class="input-group-text" >nombar</span>
+            </div>
+            <input class="" type="text" placeholder="AC2-07337-anon"></input>
+          </div>
+
+          <div class="input-group col">
+            <div class="input-group-prepend">
+              <span class="input-group-text" >nombar</span>
+            </div>
+            <input class="" type="text" placeholder="AC2-07337-anon"></input>
+          </div>
+
+        </div>  
+
+        <div className="row">
+
+          <div class="input-group col">
+            <div class="input-group-prepend">
+              <span class="input-group-text" >nombar</span>
+            </div>
+            <input class="" type="text" placeholder="AC2-07337-anon"></input>
+          </div>
+
+          <div class="input-group col">
+            <div class="input-group-prepend">
+              <span class="input-group-text" >nombar</span>
+            </div>
+            <input class="" type="text" placeholder="AC2-07337-anon"></input>
+          </div>
+
+          <div class="input-group col">
+            <div class="input-group-prepend">
+              <span class="input-group-text" >nombar</span>
+            </div>
+            <input class="" type="text" placeholder="AC2-07337-anon"></input>
+          </div>
+
+        </div>
+
+        <div className="row">
+
+          <div class="input-group col">
+            <div class="input-group-prepend">
+              <span class="input-group-text" >nombar</span>
+            </div>
+            <input class="" type="text" placeholder="AC2-07337-anon"></input>
+          </div>
+
+          <div class="input-group col">
+            <div class="input-group-prepend">
+              <span class="input-group-text" >nombar</span>
+            </div>
+            <input class="" type="text" placeholder="AC2-07337-anon"></input>
+          </div>
+
+          <div class="input-group col">
+            <div class="input-group-prepend">
+              <span class="input-group-text" >nombar</span>
+            </div>
+            <input class="" type="text" placeholder="AC2-07337-anon"></input>
+          </div>
+
+        </div> 
+
+        {/* <label class="col-lg-2 col-form-label">Autoclave title</label>
+        <input class="col-lg" type="text" placeholder="AC2-07337-anon"></input> */}
+
+        {/* <label class="col-lg-2 col-form-label">nombar</label> */}
+        
+
+        {/* <label class="col-lg-2 col-form-label">Autoclave title</label>
+        <input class="" type="text" placeholder="AC2-07337-anon"></input>
+
+        <label class="col-lg-2 col-form-label">Autoclave title</label>
+        <input class="col-lg" type="text" placeholder="AC2-07337-anon"></input>
+
+        <label class="col-lg-2 col-form-label">Autoclave title</label>
+        <input class="col-lg" type="text" placeholder="AC2-07337-anon"></input> */}
+      </form>
+
+    </div>
     
+    
+    )
   }
 
 
@@ -25,6 +155,8 @@ function App() {
       <button type="button" className="btn btn-info">Info</button>
       <button type="button" className="btn btn-light">Light</button>
       <button type="button" className="btn btn-dark">Dark</button>
+
+      {inputForm()}
     </div>
   );
 }
